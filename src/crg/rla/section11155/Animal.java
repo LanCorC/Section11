e4ec01e5fd491465f526f8657eac327036a1bf93
LanCorC <lanc3.corcega@gmail.com>
2023-02-07 18:51:05 +1300

    Completing Section 11.158: Interfaces since JDK8 (default methods, public static methods)

--- src/crg/rla/section11155/Animal.java
+++ src/crg/rla/section11155/Animal.java
@@ -8,6 +8,11 @@ enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTIO
             System.out.println("Monitoring " + this);
         }
     }
+
+    public FlightStages getNextStage() {
+        FlightStages[] allStages = values();
+        return allStages[(ordinal() + 1) % allStages.length];
+    }
 }
 record DragonFly(String name, String type) implements FlightEnabled {
 
@@ -60,6 +65,16 @@ interface FlightEnabled {
     void takeOff();
     void land();
     void fly();
+
+    default FlightStages transition(FlightStages stage) {
+//        System.out.println("transition not implemented on "
+//                + getClass().getName());
+//
+        FlightStages nextStage = stage.getNextStage();
+        System.out.println("Transitioning from " + stage + " to " +
+                nextStage);
+        return nextStage;
+    }
 }
 
 interface Trackable {
