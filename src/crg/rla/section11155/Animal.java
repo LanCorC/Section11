f92c46f9be3ab9fcd69460a809acf6d83b1e2441
LanCorC <lanc3.corcega@gmail.com>
2023-02-07 17:09:15 +1300

    Completing Section 11.157: Interfaces Part 3

--- src/crg/rla/section11155/Animal.java
+++ src/crg/rla/section11155/Animal.java
@@ -1,5 +1,58 @@
 package crg.rla.section11155;
 
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA_COLLECTION;
+
+    @Override
+    public void track() {
+        if (this != GROUNDED) {
+            System.out.println("Monitoring " + this);
+        }
+    }
+}
+record DragonFly(String name, String type) implements FlightEnabled {
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+class Satellite implements Orbitearth {
+
+    @Override
+    public void achieveOrbit() {
+        System.out.println("Orbit has been achieved.");
+    }
+
+    @Override
+    public void takeOff() {
+
+    }
+
+    @Override
+    public void land() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
+interface Orbitearth extends FlightEnabled {
+
+    void achieveOrbit();
+}
+
 interface FlightEnabled {
 
     double MILES_TO_KM = 1.60934;
